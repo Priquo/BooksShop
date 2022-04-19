a9c740b4cfcbc9fdcd406a5141f0e7f41be50bf2
Priquo <limela.arolf@gmail.com>
2022-04-19 11:42:30 +0300

    можно сделать заказ

--- BooksShop/Pages/Bascket.xaml.cs
+++ BooksShop/Pages/Bascket.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BooksShop.Classes;
+using DLLdiscounts;
 
 namespace BooksShop.Pages
 {
@@ -24,17 +25,36 @@ namespace BooksShop.Pages
         List<bookToOrder> bookToOrders;
         orders order;
         bool isBascketEmpty = false;
+        int countBook =0;
+        float cost =0;
+        float discount = 0;
+        Calctulate calctulate = new Calctulate();
         public Bascket(List<bookToOrder> bookToOrders, orders order)
         {
             InitializeComponent();
             this.bookToOrders = bookToOrders;
             this.order = order;
             listOrders.ItemsSource = bookToOrders;
+            foreach (var books in bookToOrders)
+            {
+                countBook += (int)books.count;
+                cost = (float)books.books.cost*countBook;
+            }
+            discount = calctulate.MakeDiscount(countBook, cost) * 100;
+            cost =cost + cost*calctulate.MakeDiscount(countBook, cost);
+            textCost.Text = "Итоговая цена: " + cost.ToString();
+            
         }
 
         private void buttonMakeOrder_Click(object sender, RoutedEventArgs e)
         {
-
+            bookToOrders.Clear();
+            isBascketEmpty = true;
+            listOrders.Items.Refresh();
+            MessageBox.Show("Ваш заказ готовится к исполнению! Информация по заказу\nНомер заказа: " + order.IDorder + "\nКогда забрать: на кассе сегодня\nЦена со скидкой: " + cost + "\nСкидка: " + discount +"%");
+            BaseConnector.BaseConnect.orders.Add(order);
+            BaseConnector.BaseConnect.bookToOrder.AddRange(bookToOrders);
+            BaseConnector.BaseConnect.SaveChanges();
         }
 
         private void buttonBack_Click(object sender, RoutedEventArgs e)
