2d3364f1061e450909c0e35767ffdb00a37b78d1
Priquo <limela.arolf@gmail.com>
2022-04-19 11:57:50 +0300

    добавила тестирование

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bd40fe7-274a-4fbc-8f2b-93da2d6e5780")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DLLdiscounts;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calctulate calctulate = new Calctulate();
+        [TestMethod]
+        public void MakeDiscount_BigValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_BigValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_BigValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_BigValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_CountBiggerThan()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_CountBiggerThan4()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(5, 300));
+            Assert.AreEqual(10, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_CountBiggerThan2()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(3, 400));
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_ZeroValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(0, 0));
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_BigFloatValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(15, 9999.3f));
+            Assert.AreEqual(34, result);
+        }
+        [TestMethod]
+        public void MakeDiscount_SmallValues()
+        {
+            int result = Convert.ToInt32(100 * calctulate.MakeDiscount(2, 400));
+            Assert.AreEqual(0, result);
+        }
+    }
+}
